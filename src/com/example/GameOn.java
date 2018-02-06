6f9f80c229bba011b48cc9dcc3268c5675f56a9b
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-02-06 04:33:15 -0600

    I realised, that my code was supposed to Work FOR ALL TYPES OF JSON not just the Siebel one. So my code had to go through major reconstruction. A few hours ago, I thought I just hda my test cases. But no. I was wrong. Now I just my just my cases. Hopefully. Its 4:33am. Thank you CS 126.

--- src/com/example/GameOn.java
+++ src/com/example/GameOn.java
@@ -7,14 +7,13 @@ import java.util.ArrayList;
 //https://courses.engr.illinois.edu/cs126/adventure/siebel.json
 
 public class GameOn {
-    static final String url = userInput.website();
+    static final String url = userInput.website(); //sends request to site with Json File
     static Layout currentLayout;
     private static ArrayList<String> itemsCarried = new ArrayList<String>();
 
-
     static {
         try {
-            currentLayout = URLget.makeApiRequest(url);
+            currentLayout = URLget.makeApiRequest(url);  // makes the Json files recieved be the layout
         } catch (UnirestException e) {
             e.printStackTrace();
         } catch (MalformedURLException e) {
@@ -24,20 +23,19 @@ public class GameOn {
 
     static Room currentRoom = findStartingRoom();
 
-    public GameOn() throws MalformedURLException, UnirestException {
-    }
-
+    /**
+     * This is the main method, that prints you finished as long as currentRoom is not endingRoom
+     */
     public static void main(String[] args) {
 
-        while (currentRoom != findEndingRoom()) {
-            roomStatus(currentRoom);
-
-        }
+        while (currentRoom != findEndingRoom()) printRoomStatus(currentRoom);
 
         System.out.print("You finished!");
     }
 
-
+    /**
+     * @return startingRoom from layout file
+     */
     private static Room findStartingRoom() {
         for (Room startingRoom : currentLayout.getRooms()) {
             if (startingRoom.getName().equalsIgnoreCase(currentLayout.getStartingRoom())) {
@@ -47,6 +45,9 @@ public class GameOn {
         return null;
     }
 
+    /**
+     * @return endingRoom from layout file
+     */
     private static Room findEndingRoom() {
         for (Room endingRoom : currentLayout.getRooms()) {
             if (endingRoom.getName().equalsIgnoreCase(currentLayout.getEndingRoom())) {
@@ -56,113 +57,65 @@ public class GameOn {
         return null;
     }
 
+    /**
+     * @param current takes the current Room and prints out several properties of the Room class
+     */
 
-    private static void roomStatus(Room current) {
+    private static void printRoomStatus(Room current) {
 
-
+        /*
+        The first few statements just print out information on the current room
+         */
         System.out.println(current.getDescription());
 
 
         if (current.getName().equalsIgnoreCase(currentLayout.getStartingRoom())) {
-
             System.out.println("Your journey begins here");
-
         }
+
         if (current.getName().equalsIgnoreCase((currentLayout.getEndingRoom()))) {
             System.out.println("You have reached your final destination");
         }
 
         if (current.getItems() != null) {
             for (String items : current.getItems()) {
-
                 System.out.println("You can take: " + items);
             }
         }
-
 
         for (Direction directions : current.getDirections()) {
             System.out.println("From here you can go: " + directions.getDirectionName());
         }
 
+        /*
+        This section below handles the if statements after taking in the UserInput and updates currentRoom
+        It checks inputs and checks using input.contains
+        */
         String input = userInput.playerInput();
         String firstTerm = null;
         String secondTerm = null;
-
-    if(input.contains(" ")) {
-         firstTerm = input.split("\\s+")[0];
-         secondTerm = input.split("\\s+")[1];
-    }
-
-
-
-
-
-        if (input.contains("go east")) {
-
+        /*
+        If the input has two words,then I seperate the two words to check for take/drop items
+         */
+        if (input.contains(" ")) {
+            firstTerm = input.split("\\s+")[0];
+            secondTerm = input.split("\\s+")[1];
+        }
+        if(input.contains("go".toLowerCase())) {
             for (Direction direction : current.getDirections()) {
-                if ("East".contains(direction.getDirectionName())) {
+                if (input.contains("go " + direction.getDirectionName().toLowerCase())) {
                     currentRoom = direction.getRoomAsRoom(direction.getRoom());
                 }
-            }
-
-        }
-        else if (input.contains("go west")) {
-
-            for (Direction direction : current.getDirections()) {
-                if ("West".contains(direction.getDirectionName())) {
-                    currentRoom = direction.getRoomAsRoom(direction.getRoom());
-                }
-            }
-
-        }
-       else if (input.contains("go south")) {
-
-            for (Direction direction : current.getDirections()) {
-                if ("South".contains(direction.getDirectionName())) {
-                    currentRoom = direction.getRoomAsRoom(direction.getRoom());
-                }
-            }
-
-        }
-        else if (input.contains("go up")) {
-
-            for (Direction direction : current.getDirections()) {
-                if ("Up".contains(direction.getDirectionName())) {
-                    currentRoom = direction.getRoomAsRoom(direction.getRoom());
-                }
-            }
-
-        }
-       else if (input.contains("go north") && !input.contains("east")) {
-
-            for (Direction direction : current.getDirections()) {
-                if ("North".contains(direction.getDirectionName())) {
-                    currentRoom = direction.getRoomAsRoom(direction.getRoom());
-                }
-            }
-
-        }
-       else if (input.contains("go northeast")) {
-
-            for (Direction direction : current.getDirections()) {
-                if ("NorthEast".contains(direction.getDirectionName())) {
-                    currentRoom = direction.getRoomAsRoom(direction.getRoom());
-                }
-            }
-
-        }
-       else if (input.contains("down")) {
-
-            for (Direction direction : current.getDirections()) {
-                if ("Down".contains(direction.getDirectionName())) {
-                    currentRoom = direction.getRoomAsRoom(direction.getRoom());
+                else {
+                    System.out.println("Enter valid direction");
+                    break;
                 }
             }
 
         }
 
 
-       else if (current.getItems() != null && current.getItems().contains(secondTerm) && firstTerm.equalsIgnoreCase("take")) {
+      else if (current.getItems() != null && current.getItems().contains(secondTerm) && firstTerm.equalsIgnoreCase("take")) {
             for (int i = 0; i < current.getItems().size(); i++) {
                 if (input.contains(current.getItems().get(i))) {
                     itemsCarried.add(current.getItems().get(i));
@@ -171,9 +124,7 @@ public class GameOn {
                 }
             }
 
-        }
-
-         else if (current.getItems() != null && itemsCarried != null && itemsCarried.contains(secondTerm) && firstTerm.equalsIgnoreCase("drop")) {
+        } else if (current.getItems() != null && itemsCarried != null && itemsCarried.contains(secondTerm) && firstTerm.equalsIgnoreCase("drop")) {
             for (int i = 0; i < itemsCarried.size(); i++) {
                 if (input.contains(itemsCarried.get(i))) {
                     current.getItems().add(itemsCarried.get(i));
@@ -183,10 +134,12 @@ public class GameOn {
             }
 
         }
+        /*
+        Catches bad inputs as they don't enter any if statements
+         */
         else {
-            System.out.println("This is a bad input. Try again");
+            System.out.println( "Invalid Input");
         }
-
         System.out.println(itemsCarried);
 
     }
--- src/com/example/URLget.java
+++ src/com/example/URLget.java
@@ -1,11 +1,11 @@
 /**
  * Starting with the startingRoom your game should display a message describing the player’s current
- environment. This should include five parts, with a newline separating them:
- 1. The current room’s description printed verbatim
- 2. If the room is the startingRoom print Your journey begins here
- 3. If the room is the endingRoom print You have reached your final destination
- 4. A list of the items in the current room.
- 5. A list of the directions that the player can move.
+ * environment. This should include five parts, with a newline separating them:
+ * 1. The current room’s description printed verbatim
+ * 2. If the room is the startingRoom print Your journey begins here
+ * 3. If the room is the endingRoom print You have reached your final destination
+ * 4. A list of the items in the current room.
+ * 5. A list of the directions that the player can move.
  */
 
 package com.example;
@@ -22,26 +22,7 @@ import java.net.URL;
  * Created by zilles on 9/19/17.
  */
 public class URLget {
-
-
     private static final int STATUS_OK = 200;
-
-    public static void main(String[] arguments) {
-        String url = "https://courses.engr.illinois.edu/cs126/adventure/siebel.json";
-
-
-        // Make an HTTP request to the above URL
-        try {
-            makeApiRequest(url);
-        } catch (UnirestException e) {
-//            e.printStackTrace();
-            System.out.println("Network not responding");
-        } catch (MalformedURLException e) {
-            System.out.println("Bad URL: " + url);
-        }
-
-
-    }
 
     public static Layout makeApiRequest(String url) throws UnirestException, MalformedURLException {
         Layout layout;
@@ -59,5 +40,4 @@ public class URLget {
             return layout;
         } else return null;
     }
-
 }
